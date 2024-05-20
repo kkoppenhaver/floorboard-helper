9e09d2963f1d29cb92dc6c62d978ed2b6275eb79
Keanan Koppenhaver <keanan@alphaparticle.com>
2024-05-20 00:40:07 -0500

    Init commit

---
+++ floorboard-helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Floorboard Helper
+ * Plugin URI:        https://floorboardai.com/
+ * Description:       A collection of helpers to run FloorboardAI.com
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Keanan Koppenhaver
+ * Author URI:        https://keanankoppenhaver.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       floorboard-helper
+ * Domain Path:       /languages
+ */
+
